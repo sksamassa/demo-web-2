e21c1000f4d3b13ebabf9217ddf64c621c4400f9
Sekou Samassa <sksamassa@gmail.com>
2023-04-24 21:18:56 +0500

    End of this party

--- src/main/java/com/example/demoweb/repository/PostRepository.java
+++ src/main/java/com/example/demoweb/repository/PostRepository.java
@@ -1,7 +1,7 @@
 package com.example.demoweb.repository;
 
-import com.example.demoweb.model.Post;
+import com.example.demoweb.models.Post;
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.stereotype.Repository;
 public interface PostRepository extends CrudRepository<Post, Long> {
 }
